88448ed03646ed2ed341995b4cb1b1884ed8c941
yunda1970 <yundalestaripuji@gmail.com>
2021-12-28 17:55:51 +0700

    Menambahkan algoritma untuk membuat pesawat 3D

---
+++ Kelompok_5_Pesawat_3D/main.cpp
@@ -0,0 +1,20 @@
+// File Header
+// Fungsi Init (Untuk Membuat Tempat Menggambar)
+// Variabel Kamera (Menentukan Posisi Awal Kamera)
+// Variabel Pesawat (Inisialisasi Awal)
+// Variabel Pencahayaan (Inisialisasi Awal)
+// Fungsi Reshape (Mempertahankan Bentuk Gambar)
+// Fungsi OrientMe (Pergerakkan Kamera Kanan/Kiri)
+// Fungsi moveMeFlat (Pergerakkan Kamera Maju/Mundur)
+// Fungsi Keyboard
+// Fungsi Press Key
+// Release Key
+// Fungsi Lightning (Mengaktifkan Pencahayaan)
+// Badan Pesawat
+// Membentuk Sayap Dan Ekor 3D
+// Mengatur Posisi Sayap Kiri
+// Mengatur Posisi Sayap Kanan
+// Mengatur Posisi Ekor
+// Animasi Pesawat
+// Fungsi Display (Menampilkan Output Program)
+// Memanggil Fungsi Untuk Manampilkan Ojek Di Layar
