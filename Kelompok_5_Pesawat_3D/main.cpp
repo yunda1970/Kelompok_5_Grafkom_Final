054c4c13ac36e645962232e7202d1d9d4fd33bf7
Reskal <reskalsaja552@gmail.com>
2021-12-28 22:12:43 +0800

    Menambahkan variabel pencahayaan(Inisialisasi Awal), Menambahkan Fungsi Reshape (Mempertahankan Bentuk Gambar), Menambahkan Variabel Pesawat (Inisialisasi Awal) --> Reskal

--- Kelompok_5_Pesawat_3D/main.cpp
+++ Kelompok_5_Pesawat_3D/main.cpp
@@ -23,8 +23,36 @@ float lx=0.0f,ly=0.0f,lz=-1.0f;
 int deltaMove = 0,h,w;
 
 // Variabel Pesawat (Inisialisasi Awal)
+float posY = 0, posX = 0, posZ = 0, wingDownY=0, wingDownX=0, bodyDownX=0,bodyDownY=0, angleWing=0;
+static int rotAngleX =0, rotAngleY =0, rotAngleZ =0;
+int planeMove = 0, planeBankLeft = 1, planeIdle=1;
+float angleZ = 0, angleX = 0, buildingAngle = 0,buildDown=0;
+GLUquadricObj *IDquadric;
 // Variabel Pencahayaan (Inisialisasi Awal)
+const GLfloat light_ambient[] = { 0.5f, 0.5f, 0.5f, 0.0f };
+const GLfloat light_diffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_position[] = { 0.0f, 20.0f, 10.0f, 1.0f };
+const GLfloat mat_ambient[] = { 0.7f, 0.7f, 0.7f, 1.0f };
+const GLfloat mat_diffuse[] = { 0.8f, 0.8f, 0.8f, 1.0f };
+const GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat high_shininess[] = { 100.0f };
 // Fungsi Reshape (Mempertahankan Bentuk Gambar)
+void Reshape(int w1, int h1)
+{
+	// Fungsi reshape (Menggerakkan kamera)
+	if(h1 == 0) h1 = 1;
+	w = w1;
+	h = h1;
+	ratio = 1.0f * w / h;
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glViewport(0, 0, w, h);
+	gluPerspective(45,ratio,0.1,1000);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	gluLookAt(x, y, z, x + lx,y + ly,z + lz, 0.0f,1.0f,0.0f);
+}
 // Fungsi OrientMe (Pergerakkan Kamera Kanan/Kiri)
 // Fungsi moveMeFlat (Pergerakkan Kamera Maju/Mundur)
 // Fungsi Keyboard
